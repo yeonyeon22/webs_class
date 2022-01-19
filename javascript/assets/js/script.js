dfab2dcd50b255a2fde9d14909c2bf36a21ecd3c
yeonyeon22 <yeonyeon0610@gmail.com>
2022-01-19 10:50:10 +0900

    20220119

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,13 +1,14 @@
 //결과보기 버튼을 클릭하면 경고창을 띄워주세요~
-document.querySelector(".result").addEventListener("click", function(){
-    alert("ddd")
-});
+//document.querySelector(".result").addEventListener("click", function(){
+//    alert("ddd")
+//});
 
 //다른 방법으로 식 표현하기//
 //document.querySelector(".result").addEventListener("click", () => {});//
 
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-        alert("ddd")
+       // alert("ddd") : 시험 작동
+       el.classList.toggle("active");
     });
 })
