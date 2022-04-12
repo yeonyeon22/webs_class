efe782d918b29841fb7a48deb9e23d3fc483d076
yeonyeon22 <yeonyeon0610@gmail.com>
2022-04-12 20:04:38 +0900

    20220412

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,124 @@
+
+    let AllMusic = [
+       {
+           name : "1. alfa",
+           artist : "nato alfa",
+           img : "music-1",
+           audio : "music-1"
+       },
+       {
+           name : "2. bravo",
+           artist : "nato bravo",
+           img : "music-2",
+           audio : "music-2"
+       },
+       {
+           name : "3. charlie",
+           artist : "nato charlie",
+           img : "music-3",
+           audio : "music-3"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-4",
+           audio : "music-4"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-5",
+           audio : "music-5"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-6",
+           audio : "music-6"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-7",
+           audio : "music-7"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-8",
+           audio : "music-8"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-9",
+           audio : "music-9"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-10",
+           audio : "music-10"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-11",
+           audio : "music-11"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-12",
+           audio : "music-12"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-13",
+           audio : "music-13"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-14",
+           audio : "music-14"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-15",
+           audio : "music-15"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-16",
+           audio : "music-16"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-17",
+           audio : "music-17"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-18",
+           audio : "music-18"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-19",
+           audio : "music-19"
+       },
+       {
+           name : "노래 제목 - 제목",
+           artist : "Jone Mickal",
+           img : "music-20",
+           audio : "music-20"
+       }           
+  ]
+
---
+++ layout/js/script.js
@@ -0,0 +1,14 @@
+(function($){
+    $(".submenu").css("display","block");
+
+
+
+
+
+
+// 탭메뉴
+var tabMenu = $(".notice");
+
+tabMenu.find("ul > li > ul").hide();
+    
+})(jQuery);
